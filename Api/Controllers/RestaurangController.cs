402e57be041fdce9b91d9014084f0a6018335bb8
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2018-12-13 10:30:21 +0100

    Initial commit

---
+++ Api/Controllers/RestaurangController.cs
@@ -0,0 +1,48 @@
+﻿using Api.Data;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RestaurangController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RestaurangController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<List<Restauranger>> GetRestaurants()
+        {            
+            return _context.Restauranger.ToList();
+        }
+
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Api/Models/Restauranger.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Models
+{
+    public class Restauranger
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
